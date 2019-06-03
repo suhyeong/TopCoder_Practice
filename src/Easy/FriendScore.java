078640668066ed937eb2afc0f747b145dc087c12
LeeSuHyeong <lsg1454@naver.com>
2019-06-03 23:47:11 +0900

    TOP CODER Algorithm Training 190603

---
+++ src/Easy/FriendScore.java
@@ -0,0 +1,32 @@
+package Easy;
+
+import java.util.*;
+
+public class FriendScore {
+	public int friend(String[] friends) {
+		int ans = 0;
+		int n = friends[0].length();
+		
+		for(int i=0;i<n;i++) {
+			int cnt = 0;
+			for(int j=0;j<n;j++) {
+				if(i==j)
+					continue;
+				else {
+					if(friends[i].charAt(j) == 'Y')
+						cnt++;
+					else {
+						for(int k=0;k<n;k++) {
+							if(friends[j].charAt(k) == 'Y' && friends[k].charAt(i) == 'Y') {
+								cnt++;
+								break;
+							}
+						}
+					}
+				}
+			}
+			ans = Math.max(ans, cnt);
+		}
+		return ans;
+	}
+}
